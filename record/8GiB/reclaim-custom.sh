86e51eb27a64fbef878fd98ee7b20265a50f077c
Liew Rui Yan <aethernet65535@gmail.com>
2026-06-19 14:00:09 +0800

    dama/record: add SYSTEM group to test
    The lastest test include SYSTEM group test result, which uses the
    system's built-in memory reclaimation mechanisms instead of DAMON, such
    as kswapd reclaim and direct reclaim.
    
    Signed-off-by: Liew Rui Yan <aethernet65535@gmail.com>

--- record/8GiB/reclaim-custom.sh
+++ record/8GiB/reclaim-custom.sh
@@ -24,19 +24,19 @@ echo N > /sys/module/damon_reclaim/parameters/enabled
 echo 800 > /sys/module/damon_reclaim/parameters/wmarks_high
 echo 700 > /sys/module/damon_reclaim/parameters/wmarks_mid
 echo 20 > /sys/module/damon_reclaim/parameters/wmarks_low
-echo $((10 * 1000000)) > /sys/module/damon_reclaim/parameters/min_age
+echo $((60 * 1000000)) > /sys/module/damon_reclaim/parameters/min_age
 
-rm -rf ./report/rec-fixed-$DATE/
-mkdir -p ./report/rec-fixed-$DATE/
+rm -rf ./report/rec-custom-$DATE/
+mkdir -p ./report/rec-custom-$DATE/
 
-cat /proc/vmstat | rg "refault" >> ./report/rec-fixed-$DATE/refault.txt
-cat /proc/vmstat | rg "pgsteal" >> ./report/rec-fixed-$DATE/pgsteal.txt
-cat /sys/module/damon_reclaim/parameters/bytes_reclaimed_regions >> ./report/rec-fixed-$DATE/damon_passed.txt
-sar -r 5 $((($TEST_MINS*60)/5)) >> ./report/rec-fixed-$DATE/memu.txt &
-sar -q CPU 5 $((($TEST_MINS*60)/5)) >> ./report/rec-fixed-$DATE/cpu.txt &
-sar -q IO 5 $((($TEST_MINS*60)/5)) >> ./report/rec-fixed-$DATE/io.txt &
-sar -q MEM 5 $((($TEST_MINS*60)/5)) >> ./report/rec-fixed-$DATE/memo.txt &
-sar -B 5 $((($TEST_MINS*60)/5)) >> ./report/rec-fixed-$DATE/fault.txt &
+cat /proc/vmstat | rg "refault" >> ./report/rec-custom-$DATE/refault.txt
+cat /proc/vmstat | rg "pgsteal" >> ./report/rec-custom-$DATE/pgsteal.txt
+cat /sys/module/damon_reclaim/parameters/bytes_reclaimed_regions >> ./report/rec-custom-$DATE/damon_passed.txt
+sar -r 5 $((($TEST_MINS*60)/5)) >> ./report/rec-custom-$DATE/memu.txt &
+sar -q CPU 5 $((($TEST_MINS*60)/5)) >> ./report/rec-custom-$DATE/cpu.txt &
+sar -q IO 5 $((($TEST_MINS*60)/5)) >> ./report/rec-custom-$DATE/io.txt &
+sar -q MEM 5 $((($TEST_MINS*60)/5)) >> ./report/rec-custom-$DATE/memo.txt &
+sar -B 5 $((($TEST_MINS*60)/5)) >> ./report/rec-custom-$DATE/fault.txt &
 
 echo Y > /sys/module/damon_reclaim/parameters/enabled
 
@@ -52,7 +52,7 @@ echo Y > /sys/module/damon_reclaim/parameters/enabled
 
 /home/user/workspace/github/masim/masim /home/user/workspace/github/dama/record/8GiB/masim/foobar_full.cfg
 
-cat /proc/vmstat | rg "refault" >> ./report/rec-fixed-$DATE/refault.txt
-cat /proc/vmstat | rg "pgsteal" >> ./report/rec-fixed-$DATE/pgsteal.txt
-cat /sys/module/damon_reclaim/parameters/bytes_reclaimed_regions >> ./report/rec-fixed-$DATE/damon_passed.txt
+cat /proc/vmstat | rg "refault" >> ./report/rec-custom-$DATE/refault.txt
+cat /proc/vmstat | rg "pgsteal" >> ./report/rec-custom-$DATE/pgsteal.txt
+cat /sys/module/damon_reclaim/parameters/bytes_reclaimed_regions >> ./report/rec-custom-$DATE/damon_passed.txt
 echo N > /sys/module/damon_reclaim/parameters/enabled
