0d51517e6b56755798bbc1948f99993ea646ab7a
Liew Rui Yan <aethernet65535@gmail.com>
2026-07-08 18:32:14 +0800

    test: change test mode
    Signed-off-by: Liew Rui Yan <aethernet65535@gmail.com>

--- record/8GiB/reclaim-dama.sh
+++ record/8GiB/reclaim-dama.sh
@@ -6,9 +6,8 @@
 MIN_SEC=60
 
 DIR_NAME="rec-dama"
-DATE="2026-07-06-0001"
+DATE="2026-07-08-0001"
 TEST_SECS=$((3600))
-TOTAL_MEM=$((15991316))
 INTERVAL_SECS=$((5))
 SAMPLING_TIMES=$(($TEST_SECS/$INTERVAL_SECS))
 
@@ -30,6 +29,16 @@ echo 120000000 > /sys/module/damon_reclaim/parameters/min_age
 rm -rf ./report/$DIR_NAME\-$DATE/
 mkdir -p ./report/$DIR_NAME\-$DATE/
 
+pushd /home/user/workspace/ycsb
+./bin/ycsb run mongodb -s \
+    -P workloads/custom/qwen \
+    -p mongodb.url=mongodb://127.0.0.1:27017/ycsb \
+    -p mongodb.maxconnections=128 \
+    -p maxexecutiontime=$TEST_SECS \
+    > /home/user/workspace/dama/record/8GiB/report/$DIR_NAME\-$DATE/ycsb.txt 2>&1 &
+popd
+sleep 10m
+
 cat /proc/vmstat | rg "refault" >> ./report/$DIR_NAME\-$DATE/refault.txt
 cat /proc/vmstat | rg "pgsteal" >> ./report/$DIR_NAME\-$DATE/pgsteal.txt
 cat /sys/module/damon_reclaim/parameters/bytes_reclaimed_regions >> ./report/$DIR_NAME\-$DATE/damon_passed.txt
@@ -39,18 +48,8 @@ sar -q IO $INTERVAL_SECS $SAMPLING_TIMES >> ./report/$DIR_NAME\-$DATE/io.txt &
 sar -q MEM $INTERVAL_SECS $SAMPLING_TIMES >> ./report/$DIR_NAME\-$DATE/memo.txt &
 sar -B $INTERVAL_SECS $SAMPLING_TIMES >> ./report/$DIR_NAME\-$DATE/fault.txt &
 
-/home/user/workspace/dama/dama >> ./report/$DIR_NAME\-$DATE/dama.txt &
-
 echo Y > /sys/module/damon_reclaim/parameters/enabled
-
-pushd /home/user/workspace/ycsb
-./bin/ycsb run mongodb -s \
-    -P workloads/custom/qwen \
-    -p mongodb.url=mongodb://127.0.0.1:27017/ycsb \
-    -p mongodb.maxconnections=128 \
-    -p maxexecutiontime=$TEST_SECS \
-    > /home/user/workspace/dama/record/8GiB/report/$DIR_NAME\-$DATE/ycsb.txt 2>&1
-popd
+sleep 60m
 
 cat /proc/vmstat | rg "refault" >> ./report/$DIR_NAME\-$DATE/refault.txt
 cat /proc/vmstat | rg "pgsteal" >> ./report/$DIR_NAME\-$DATE/pgsteal.txt
