0ec7fa53e0708d14c0f4019a76a74acb11357a9c
Liew Rui Yan <aethernet65535@gmail.com>
2026-07-25 18:44:34 +0800

    update
    Signed-off-by: Liew Rui Yan <aethernet65535@gmail.com>

---
+++ scripts/fedora_install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ "$(id -u)" -ne 0 ]; then
+  echo "please run as root"
+  exit 1
+fi
+
+echo "============================="
+echo "DAMA FEDORA 44 INSTALL SCRIPT"
+echo "============================="
+
+dnf install \
+make \
+gcc \
+ollama \
+sysstat \
+python3
+
+ollama pull gemma3:4b
+
+systemctl enable --now sysstat
